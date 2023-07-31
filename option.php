1b209cb98e47597b9e03f36fcfde6579692faa7d
S1NESTRE4 <116363805+SinestreaPukk@users.noreply.github.com>
2023-07-31 21:41:54 +0700

    Create option.php

---
+++ option.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Online Courses</title>
+
+
+    <link rel="stylesheet" href="styles.css">
+    <script defer src="app.js"></script>
+
+</head>
+
+
+
+<body>
+<nav class="navbar">
+    <div class="navbar__container">
+
+
+        <a href="index.php" id="navbar__logo"> Project Z</a>
+        
+        <div class="navbar__toggle" id="mobile-menu">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+
+        <ul class="navbar__menu">
+            
+
+            <li class="navbar__item">
+                <a href="AboutUs.php" class="navbar__links">About Us</a>
+            </li>
+
+            <li class="navbar__item">
+                <a href="Contact.html" class="navbar__links">Resources</a>
+            </li>
+
+            <li class="navbar__item">
+                <a href="Support.php" class="navbar__links">Support Us</a>
+            </li>
+            
+            <li class="navbar__item">
+                <a href="https://sites.google.com/regents.ac.th/parents/home?authuser=0" class="navbar__links">Regent Website</a>
+            </li>
+            
+            <li class="navbar__btn">
+                <a href="register_form.php" class="button">SignUp</a>
+            </li>
+
+    
+        </ul>
+    </div>
+</nav>
+
+
+<header class="course_header">
+    <h1>Courses Page</h1>
+</header>
+
+
+
+
+<main class="subject_main">
+    <section class="course">
+      <img src="course1.jpg" alt="Course 1">
+      <h2>(IGCSE) Math</h2>
+      <p>Description of Course 1.</p>
+      <a href="Math_DPM.php" class="btn">Let's Start</a>
+    </section>
+
+    <section class="course">
+      <img src="course2.jpg" alt="Course 2">
+      <h2>(IGCSE) Physic</h2>
+      <p>Description of Course 2.</p>
+      <a href="#" class="btn">Let's Start</a>
+    </section>
+
+    <section class="course">
+      <img src="course3.jpg" alt="Course 3">
+      <h2>(IGCSE) Chemistry</h2>
+      <p>Description of Course 3.</p>
+      <a href="#" class="btn">Let's Start</a>
+    </section>
+
+    <section class="course">
+      <img src="course4.jpg" alt="Course 4">
+      <h2>(IGCSE) Biology</h2>
+      <p>Description of Course 4.</p>
+      <a href="#" class="btn">Let's Start</a>
+    </section>
+
+    <section class="course">
+      <img src="course5.jpg" alt="Course 5">
+      <h2>(IGCSE) English Language</h2>
+      <p>Description of Course 5.</p>
+      <a href="#" class="btn">Let's Start</a>
+    </section>
+
+    <section class="course">
+      <img src="course6.jpg" alt="Course 6">
+      <h2>(IGCSE) English Literature</h2>
+      <p>Description of Course 6.</p>
+      <a href="#" class="btn">Let's Start</a>
+    </section>
+
+    <section class="course">
+      <img src="course7.jpg" alt="Course 7">
+      <h2>(IGCSE) Computer Science</h2>
+      <p>Description of Course 7.</p>
+      <a href="#" class="btn">Let's Start</a>
+    </section>
+
+    <section class="course">
+      <img src="course8.jpg" alt="Course 8">
+      <h2>(IGCSE) Design Technology</h2>
+      <p>Description of Course 8.</p>
+      <a href="#" class="btn">Let's Start</a>
+    </section>
+
+    <section class="course">
+      <img src="course9.jpg" alt="Course 9">
+      <h2>(IGCSE) Physical Education</h2>
+      <p>Description of Course 9.</p>
+      <a href="#" class="btn">Let's Start</a>
+    </section>
+
+    <section class="course">
+      <img src="10.jpg" alt="Course 10">
+      <h2> SAT Math preperation</h2>
+      <p>Description of Course 10.</p>
+      <a href="#" class="btn">Let's Start</a>
+    </section>
+
+    <section class="course">
+      <img src="course11.jpg" alt="Course 11">
+      <h2>SAT English preperation</h2>
+      <p>Description of Course 11.</p>
+      <a href="#" class="btn">Let's Start</a>
+    </section>
+
+
+    <!-- Add more course sections as needed -->
+</main>
+
+
+
+</body>
+
+
+
+    
+    
+</html>
